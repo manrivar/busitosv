fa43cb45a89007c02c1e9d0fa7efe3381373a8c2
Manuel Rivera Argueta <manrivargueta@gmail.com>
2022-06-05 02:16:52 -0600

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Busitosv
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                Lorem ipsum dolor sit amet consectetur.
+                Voluptatem saepe eaque impedit quam suscipit.
+                Natus, laudantium? Pariatur consequuntur doloribus voluptatem.
+                Illo, ut libero? Repudiandae, earum soluta!
+                Deleniti accusantium distinctio nesciunt ea quo.
+                Nisi id voluptates quam harum neque?
+                Illum obcaecati est numquam aspernatur earum.
+                Fuga voluptate velit amet harum nostrum.
+                Exercitationem, iusto unde? Adipisci, magni iste?
+                Itaque, cumque! Delectus tempora eaque excepturi!
+                Minus nemo ratione nulla consequatur enim.
+                Deserunt laborum rem quia odit enim.
+                Vitae voluptates culpa in iusto doloremque.
+                Minima quia neque similique exercitationem sint.
+                Cum itaque corrupti quo nulla dolor!
+                Repudiandae et excepturi earum nobis expedita!
+                Nam minima vitae a inventore quisquam?
+                Natus soluta laborum possimus eaque voluptatibus?
+                Inventore blanditiis harum nisi iste at.
+                Eum mollitia deleniti explicabo ratione dolorum?
+                Reiciendis dolores quos laudantium. Iusto, officia!
+                Aliquam quo quas beatae soluta ad!
+                Ab, quasi iure. Odit, numquam quos!
+                Eos minima voluptas illum voluptatem labore?
+                Fuga neque doloremque soluta numquam ut?
+                Quas aspernatur placeat ab similique saepe.
+                Qui aspernatur quas eum nobis veritatis.
+                Tempora dolor eaque obcaecati repellendus accusamus!
+                Rerum unde quae cumque earum maiores?
+                Exercitationem dolor velit atque vero magnam.
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('storage/logo/busitosv-logo.png')}}" class="logo" />
