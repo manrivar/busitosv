830d82b28e46e818ece943dda64c749cebbb0723
Manuel Rivera Argueta <manrivargueta@gmail.com>
2022-07-07 22:28:27 -0600

    combinados

--- resources/views/sobre_nosotros.blade.php
+++ resources/views/sobre_nosotros.blade.php
@@ -1,3 +1,384 @@
 <x-app-layout>
-
+    <!DOCTYPE html>
+    <html>
+    
+    <head>
+      <!-- Basic -->
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <!-- Site Metas -->
+      <meta name="keywords" content="" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+    
+      <title>BUSITO SV</title>
+    
+      <!-- slider stylesheet -->
+      <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+    
+      <!-- bootstrap core css -->
+      <link rel="stylesheet" type="text/css" href="/Landing/css/bootstrap.css" />
+    
+      <!-- fonts style -->
+      <link href="https://fonts.googleapis.com/css?family=Dosis:400,500|Poppins:400,700&display=swap" rel="stylesheet">
+      <!-- Custom styles for this template -->
+      <link href="/Landing/css/style.css" rel="stylesheet" />
+      <!-- responsive style -->
+      <link href="/Landing/css/responsive.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+      <div class="hero_area">
+        <!-- end header section -->
+        <!-- slider section -->
+        <section class=" slider_section position-relative">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-4 offset-md-2">
+                      <div class="slider_detail-box">
+                        <h1>
+                        Lorem Ipsum
+                          <span>
+                          Lorem Ipsum
+                          </span>
+                        </h1>
+                        <p>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                          Lorem Ipsum has been the industry's standard dummy text ever
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="slider_img-box">
+                        <img src="/Landing/images/slider-img.jpg" alt="" style="height:400px !important; width:400px !important;">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-4 offset-md-2">
+                      <div class="slider_detail-box">
+                        <h1>
+                        Lorem Ipsum
+                          <span>
+                          Lorem Ipsum
+                          </span>
+                        </h1>
+                        <p>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                          Lorem Ipsum has been the industry's standard dummy text ever
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="slider_img-box">
+                        <img src="/Landing/images/slider-img.jpg" alt="" style="height:400px !important; width:400px !important;">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-4 offset-md-2">
+                      <div class="slider_detail-box">
+                        <h1>
+                        Lorem Ipsum
+                          <span>
+                          Lorem Ipsum
+                          </span>
+                        </h1>
+                        <p>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                          Lorem Ipsum has been the industry's standard dummy text ever
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="slider_img-box">
+                        <img src="/Landing/images/slider-img.jpg" alt="" style="height:400px !important; width:400px !important;">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-4 offset-md-2">
+                      <div class="slider_detail-box">
+                        <h1>
+                        Lorem Ipsum
+                          <span>
+                          Lorem Ipsum
+                          </span>
+                        </h1>
+                        <p>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                          Lorem Ipsum has been the industry's standard dummy text ever
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="slider_img-box">
+                        <img src="/Landing/images/slider-img.jpg" alt="" style="height:400px !important; width:400px !important;">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    
+        </section>
+        <!-- end slider section -->
+      </div>
+    
+      <!-- about section -->
+    
+      <section id="rutas" class="about_section layout_padding">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="img-box">
+                <img src="/Landing/images/about.jpg" alt="" style="height:400px !important;">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="detail-box">
+                <h2 class="custom_heading">
+                  Rutas
+                  <span>
+                    Buses y Microbuses
+                  </span>
+                </h2>
+                <p>
+                Visitar San Salvador nunca ha sido tan fácil. Obtenga indicaciones paso a paso sobre la marcha de las rutas
+                de buses o microbuses a cualquier destino, calle o estación del Gran San Salvador.
+                </p>
+                <div>
+                  <a href="">
+                    Más información...
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- service section -->
+      <section id="conocenos" class="service_section layout_padding">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6 offset-md-2">
+              <h2 class="custom_heading">
+                Conocenos | <span> BUSITO SV</span>
+              </h2>
+              <div class="container layout_padding2">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="img_box">
+                      <img src="/Landing/images/s-1.png" alt="">
+                    </div>
+                    <div class="detail_box">
+                      <h6>
+                        ¡Busca!
+                      </h6>
+                      <p>
+                        onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exe
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="img_box">
+                      <img src="/Landing/images/s-2.png" alt="">
+                    </div>
+                    <div class="detail_box">
+                      <h6>
+                        ¡Elige!
+                      </h6>
+                      <p>
+                        onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exe
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="img_box">
+                      <img src="/Landing/images/s-3.png" alt="">
+                    </div>
+                    <div class="detail_box">
+                      <h6>
+                        ¡Viaja!
+                      </h6>
+                      <p>
+                        onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exe
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <img src="/Landing/images/logob.png" alt="" class="w-70">
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- end service section -->
+    
+      <!-- map section -->
+    
+      <section id="contactanos" class="map_section">
+        <div id="map" class="h-100 w-100 ">
+        </div>
+    
+        <div class="form_container ">
+          <div class="row">
+            <div class="col-md-8 col-sm-10 offset-md-4">
+              <form action="">
+                <div class="text-center">
+                  <h3>
+                    ¡Contactanos!
+                  </h3>
+                </div>
+                <div>
+                  <input type="text" placeholder="Nombre" class="pt-3">
+                </div>
+                <div>
+                  <input type=" text" placeholder="Número de Teléfono">
+                </div>
+                <div>
+                  <input type="email" placeholder="Email">
+                </div>
+                <div>
+                  <input type="text" class="message-box" placeholder="Mensaje">
+                </div>
+                <div class="d-flex justify-content-center">
+                  <button>
+                    Enviar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
+    
+    
+      <!-- end map section -->
+    
+      <!-- info section -->
+      <section class="info_section layout_padding2">
+        <div class="container">
+          <div class="info_items">
+            <a href="">
+              <div class="item ">
+                <div class="img-box box-1">
+                  <img src="" alt="">
+                </div>
+                <div class="detail-box">
+                  <p>
+                    San Salvador, El Salvador
+                  </p>
+                </div>
+              </div>
+            </a>
+            <a href="">
+              <div class="item ">
+                <div class="img-box box-2">
+                  <img src="" alt="">
+                </div>
+                <div class="detail-box">
+                  <p>
+                    +503 2257 7777
+                  </p>
+                </div>
+              </div>
+            </a>
+            <a href="">
+              <div class="item ">
+                <div class="img-box box-3">
+                  <img src="" alt="">
+                </div>
+                <div class="detail-box">
+                  <p>
+                    busitosv@gmail.com
+                  </p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
+    
+      <!-- end info_section -->
+    
+      <!-- footer section -->
+      <section class="container-fluid footer_section">
+        <p>
+          &copy; 2022 All Rights Reserved By
+          <a href="https://html.design/">BUSITO SV</a>
+        </p>
+      </section>
+      <!-- footer section -->
+    
+      <script type="text/javascript" src="/Landing/js/jquery-3.4.1.min.js"></script>
+      <script type="text/javascript" src="/Landing/js/bootstrap.js"></script>
+    
+      <script>
+        // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+        // Australia.
+        function initMap() {
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: {
+              lat: 13.71481290691618, 
+              lng: -89.23896308516025
+            },
+          });
+          var image = '/Landing/images/maps-and-flags.png';
+          var beachMarker = new google.maps.Marker({
+            position: {
+              lat: 13.71481290691618, 
+              lng: -89.23896308516025
+            },
+            map: map,
+            icon: image
+          });
+        }
+      </script>
+      <!-- google map js -->
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap">
+      </script>
+      <!-- end google map js -->
+    </body>
+    </body>
+    
+    </html>
 </x-app-layout>
